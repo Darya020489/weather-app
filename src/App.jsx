af114d7f351eb4de8a45f9995bab77b5bb9cd0c7
Daria <daria7@tut.by>
2022-12-13 23:47:23 +0300

    added styles to AdditionalInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Favicon from 'react-favicon';
 import TownWeather from "./components/TownWeather";
 import MainLayout from "./layouts/MainLayout";
 
@@ -7,6 +8,7 @@ import { Route, Routes } from "react-router";
 function App() {
   return (
     <div className="App">
+      <Favicon url='https://cdn3.iconfinder.com/data/icons/luchesa-vol-9/128/Weather-1024.png' />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="townWeather/:name" element={<TownWeather />} />
--- src/components/town/AdditionalInfo.jsx
+++ src/components/town/AdditionalInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Flex from '../Flex';
+import {getTemperature} from '../../functions/getTemperature';
 import day from "../../assets/images/additionalInfoIcons/day.svg";
 import night from "../../assets/images/additionalInfoIcons/night.svg";
 import wind from "../../assets/images/additionalInfoIcons/wind.svg";
@@ -10,54 +11,88 @@ import humidity from "../../assets/images/additionalInfoIcons/humidity.svg";
 import visibility from "../../assets/images/additionalInfoIcons/visibility.svg";
 
 const MoreInfo = styled.div`
-h3{
+h4{
+  margin-top: 10px;
   text-align: left;
 }
   img{
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
+  }
+  .more-info__param{
+font-size: 14px;
+  }
+  .more-info__value{
+    font-size: 14px;
+    font-weight: 500;
+    color: rgb(55, 130, 220);
   }
 `;
 
 function AdditionalInfo() {
 const info = useSelector(state => state.townWeather.weatherForecast.list[0]);
 
+const measure = useSelector((state) => state.indicators.measure);
+const temperatureMax = getTemperature(info.main.temp_max, measure);
+const temperatureMin = getTemperature(info.main.temp_min, measure);
+
+const degValue = info.wind.deg;
+let degWind = '';
+if(degValue >=0 && degValue < 10){
+  degWind = 'North';
+}else if(degValue >= 10 && degValue < 80){
+  degWind = 'North-east';
+}else if(degValue >= 80 && degValue < 100){
+  degWind = 'East';
+}else if(degValue >= 100 && degValue < 170){
+  degWind = 'South-East';
+}else if(degValue >= 170 && degValue < 190){
+  degWind = 'South';
+}else if(degValue >= 190 && degValue < 260){
+  degWind = 'South-West';
+}else if(degValue >= 260 && degValue < 280){
+  degWind = 'West';
+}else if(degValue >= 280 && degValue < 350){
+  degWind = 'North-west';
+}else{
+  degWind = 'North';
+}
 
   return (
     <MoreInfo>
-      <h3>More info</h3>
-      <Flex justify="space-between">
+      <h4>More info</h4>
+      <Flex justify="space-between" align='start'>
       <Flex direction="column" margin="10px">
-        <p>Day</p>
+        <p className="more-info__param">Day</p>
         <img src={day} alt="" />
-        <p>{info.main.temp_max}</p>
+        <p className="more-info__value">{temperatureMax}{measure}</p>
       </Flex>
       <Flex direction="column" margin="10px">
-        <p>Night</p>
+        <p className="more-info__param">Night</p>
         <img src={night} alt="" />
-        <p>{info.main.temp_min}</p>
+        <p className="more-info__value">{temperatureMin}{measure}</p>
       </Flex>
       <Flex direction="column" margin="10px">
-        <p>Wind</p>
+        <p className="more-info__param">Wind</p>
         <img src={wind} alt="" />
-        <p>{info.wind.speed}</p>
+        <p className="more-info__value">{info.wind.speed}m/s,<br/>{degWind}</p>
       </Flex>
       </Flex>
-      <Flex justify="space-between">
+      <Flex justify="space-between" align='start'>
       <Flex direction="column" margin="10px">
-        <p>Clouds</p>
+        <p className="more-info__param">Clouds</p>
         <img src={clouds} alt="" />
-        <p>{info.clouds.all}</p>
+        <p className="more-info__value">{info.clouds.all}%</p>
       </Flex>
       <Flex direction="column" margin="10px">
-        <p>Humidity</p>
+        <p className="more-info__param">Humidity</p>
         <img src={humidity} alt="" />
-        <p>{info.main.humidity}</p>
+        <p className="more-info__value">{info.main.humidity}%</p>
       </Flex>
       <Flex direction="column" margin="10px">
-        <p>Visibility</p>
+        <p className="more-info__param">Visibility</p>
         <img src={visibility} alt="" />
-        <p>{info.visibility}</p>
+        <p className="more-info__value">{info.visibility}m</p>
       </Flex>
       </Flex>
       
